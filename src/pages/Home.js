aaff6c88ce2fb2f6dfb41b0698be78f59fab7220
Pedro Wilson <pdro.wilson@gmail.com>
2021-09-10 23:28:11 -0300

    sidebar deleted

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,10 @@
 import Header from '../components/Header'
-import Sidebar from '../components/Sidebar'
 import Watchlist from '../components/Watchlist'
 
 function Home() {
   return (
     <div>
       <Header />
-      <Sidebar />
       <Watchlist />
     </div>
   )
