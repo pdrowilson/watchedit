974a79ff2b60cd263fb325e130463686845791b2
Pedro Wilson <pdro.wilson@gmail.com>
2021-09-12 01:25:28 -0300

    updated featured movie component

--- src/components/Featured/index.js
+++ src/components/Featured/index.js
@@ -1,8 +1,26 @@
 import * as S from './styles'
+import img from '../../assets/img-movie.jpg'
+import { Button } from '@material-ui/core'
+
 function Featured() {
   return (
     <S.FeaturedContainer>
-      <h1>Featured Movies</h1>
+      <S.GridWrapper>
+        <S.InfoContainer>
+          <p>2021</p>
+          <h1>Once Upon a Time... In Hollywood</h1>
+          <div>
+            <h2>7.7</h2>
+            <span>10</span>
+          </div>
+          <div>
+            <Button>Add to Watchlist</Button>
+          </div>
+        </S.InfoContainer>
+        <S.ImgContainer>
+          <img src={img} alt="img" />
+        </S.ImgContainer>
+      </S.GridWrapper>
     </S.FeaturedContainer>
   )
 }
