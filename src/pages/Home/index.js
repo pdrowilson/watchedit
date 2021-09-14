af15086c7f8c0abd7a895a11642c7d84a6e1fea7
Pedro Wilson <pdro.wilson@gmail.com>
2021-09-14 08:50:31 -0300

    added slider feature

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,32 +2,14 @@ import circle from '../../assets/circle.png'
 import circleLines from '../../assets/circle-lines.png'
 import * as S from './styles'
 
-import Featured from '../../components/Featured'
+import Slider from '../../components/Slider'
 
-import { useTrending } from '../../hooks/useTrending'
-
-import { useEffect } from 'react'
 function Home() {
-  const { trendingMovieList, trendingMovies } = useTrending()
-  console.log(trendingMovieList, 'trendingMovieList')
-
-  useEffect(() => {
-    async function getTrendingList() {
-      try {
-        await trendingMovies()
-      } catch (error) {
-        alert('Ocorreu um erro ao buscar os items')
-      }
-    }
-    getTrendingList()
-  }, [])
   return (
     <S.HomeContainer>
       <img src={circle} alt="img" />
       <img src={circleLines} alt="img" />
-      {trendingMovieList.map((movie, key) => (
-        <Featured key={key} movie={movie} />
-      ))}
+      <Slider />
     </S.HomeContainer>
   )
 }
